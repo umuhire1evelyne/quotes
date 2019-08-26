e99e1cb30e63e61bd93e039dd4cfe6ecd2630571
umuhire1evelyne <umuhire1evelyne@gmail.com>
2019-08-26 15:29:41 +0200

    quote form.ts was added

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,18 +1,18 @@
-// import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-// @Component({
-//   selector: 'app-quote-form',
-//   templateUrl: './quote-form.component.html',
-//   styleUrls: ['./quote-form.component.css']
-// })
-// export class QuoteFormComponent implements OnInit {
+@Component({
+  selector: 'app-quote-form',
+  templateUrl: './quote-form.component.html',
+  styleUrls: ['./quote-form.component.css']
+})
+export class QuoteFormComponent implements OnInit {
 
-//   constructor() { }
+  constructor() { }
 
-//   ngOnInit() {
-//   }
+  ngOnInit() {
+  }
 
-// }
+}
 
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
